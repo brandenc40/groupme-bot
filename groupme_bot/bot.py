57a8d64e6c679a1fe84d1fef8001c1b50690c497
Branden <37084158+brandenc40@users.noreply.github.com>
2020-12-22 16:18:41 -0600

    Update bot.py

--- groupme_bot/bot.py
+++ groupme_bot/bot.py
@@ -46,7 +46,22 @@ class Bot:
         return decorator
 
     def cron_task(self, *args, **kwargs):
-        """A decorator that registers a function to be run on set cron schedule"""
+        """A decorator that registers a function to be run on set cron schedule
+        
+        Available arguments (https://apscheduler.readthedocs.io/en/stable/modules/triggers/cron.html):
+            - year (int|str) – 4-digit year
+            - month (int|str) – month (1-12)
+            - day (int|str) – day of the (1-31)
+            - week (int|str) – ISO week (1-53)
+            - day_of_week (int|str) – number or name of weekday (0-6 or mon,tue,wed,thu,fri,sat,sun)
+            - hour (int|str) – hour (0-23)
+            - minute (int|str) – minute (0-59)
+            - second (int|str) – second (0-59)
+            - start_date (datetime|str) – earliest possible date/time to trigger on (inclusive)
+            - end_date (datetime|str) – latest possible date/time to trigger on (inclusive)
+            - timezone (datetime.tzinfo|str) – time zone to use for the date/time calculations (defaults to scheduler timezone)
+            - jitter (int|None) – advance or delay the job execution by jitter seconds at most.
+        """
         def decorator(f):
             self._cron_functions.append({'args': args, 'kwargs': kwargs, 'f': f})
             return f
